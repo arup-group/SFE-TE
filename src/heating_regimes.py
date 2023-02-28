afe8e3b2e7ad3ca74c0565d110b2d063d4e9fc9d
Yavor Panev <yavor.panev@arup.com>
2023-02-28 10:45:16 +0000

    added standard fire curve

---
+++ src/heating_regimes.py
@@ -0,0 +1,48 @@
+"""
+This module contains standard heating regimes used for time equivalence
+"""
+
+import numpy as np
+
+class GenericRegime:
+    """Generic class"""
+
+    def __init__(self):
+        self.label = 'Generic'
+        self.descr = 'Generic descr'
+
+    def get_temp(self):
+        raise NotImplemented
+
+    def get_delta_temp(self):
+        raise NotImplemented
+
+
+class StandardFire(GenericRegime):
+    """Standard fire curve"""
+
+    def __init__(self):
+        self.label = 'ISO 834'
+        self.descr = 'Standard fire heating curve to ISO 834'
+
+    def get_temp(self, t):
+        """ Gets a temperature reading(s) from ISO 834 curve. UNIT TEST REQUIRED.
+        Inputs:
+            t(float, array-like): time in [s]
+        Returns:
+            T(float, array-like): Temperature in [degC] """
+
+        if isinstance(t, list):
+            t = np.asarray(t)
+        return 20 + 345*np.log10(8*t/60+1)
+
+    def get_delta_temp(self, t1, t2):
+
+        """Gets difference in temperature at two points in time
+         where t2>t1 """
+
+        return self.get_temp(t2) - self.get_temp(t1)
+
+
+
+
