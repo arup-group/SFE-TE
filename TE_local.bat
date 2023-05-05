3f390da71b4444a64f79fc0ba8b347e58231263e
Yavor Panev <yavor.panev@arup.com>
2023-05-05 11:06:59 +0100

    added main function and local Windows starter

---
+++ TE_local.bat
@@ -0,0 +1,11 @@
+@echo off
+pushd %~dp0
+echo Welcome to TE tool local starter
+echo[
+set /p "inputFile=Enter path to input file: "
+set /p "outputDir=Enter path to output directory: "
+echo[
+call conda activate fire_general
+python src/main.py -i %inputFile% -o %outputDir%
+call conda deactivate
+pause
--- src/main.py
+++ src/main.py
@@ -1,15 +1,41 @@
 import case_control as cc
+import argparse
 
-print('*** SOME - NAME - OF - THE - TOOL 0.1 beta ***')
-print('SFE tool for probabilistic design fire generation and  quantified risk assessment\n')
-print('Special thanks to UKMEA Fire team, AWF and TDA dev teams, as well as Arup Digital Strategic Development Fund.')
-print('Contact: Yavor Panev at yavor.panev@arup.com\n')
-print('*** Analysis Start ***\n')
 
-analysis = cc.CaseControler(inputs=r'samples\test_input.json', out_f=r'dump')
-analysis.initiate_methods()
-analysis.conduct_pre_run_calculations()
-analysis.run_a_study()
-analysis.run_b_study()
+def get_args():
+    parser = argparse.ArgumentParser(description='Tutorial workflow for AWF.')
+    parser.add_argument("-i", "--inputFile", help="path to input json file")
+    parser.add_argument("-o", "--outputDir", help="path to output directory")
+    args = parser.parse_args()
+    return args
 
-print('*** Analysis Completed Successfully ***\n')
+
+def main():
+    """Method contains the main program flow. Refer to TGN for more guidance."""
+
+    print('*** SOME - NAME - OF - THE - TOOL 0.1 beta ***')
+    print('SFE tool for probabilistic design fire generation and  quantified risk assessment\n')
+    print(
+        'Special thanks to UKMEA Fire team, AWF and TDA dev teams, as well as Arup Digital Strategic Development Fund.')
+    print('Contact: Yavor Panev at yavor.panev@arup.com\n')
+    print('*** Analysis Start ***\n')
+
+    args = get_args()
+
+    # Method fetches all inputs and configurations. Sets up the folder space
+    analysis = cc.CaseControler(inputs=args.inputFile, out_f=args.outputDir)
+    analysis.initiate_methods()
+
+    # This includes: 1) Calculation of protection requirements for different standard exposure
+    # 2) Assessment of risk target
+    analysis.conduct_pre_run_calculations()
+    # Analysis of all cases according to run a setup parameters
+    analysis.run_a_study()
+    # Analysis of max, min, and median case according to run b setup parameters
+    analysis.run_b_study()
+
+    print('*** Analysis Completed Successfully ***\n')
+
+
+if __name__ == "__main__":
+    main()
