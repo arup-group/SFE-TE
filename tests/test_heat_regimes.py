5e511af4f22ef3af29409ab00e94462ef848d89c
Yavor Panev <yavor.panev@arup.com>
2023-09-12 18:23:13 +0100

    finished unit test cases for EC1 method parameters

--- tests/test_heat_regimes.py
+++ tests/test_heat_regimes.py
@@ -8,17 +8,17 @@ class TestUniEC1(unittest.TestCase):
 
     def setUp(self):
         inputs = {
-            'A_c': np.array([40, 60, 1000]),
-            'c_ratio': np.array([0.2, 1, 0]),
-            'h_c': np.array([3, 4, 0]),
-            'w_frac': np.array([0.15, 0.5, 0]),
-            'remain_frac': np.array([0.0, 0.1, 0]),
-            'q_f_d': np.array([300, 300, 0]),
-            't_lim': np.array([15, 20, 0]),
-            'w_frac_h': np.array([0.5, 0.75, 0]),
-            'fabr_inrt' : np.array([1000, 1000, 0]),
-            'foo': np.array([1000, 1000, 0])}
-        crit_value = 100
+            'A_c': np.array([40, 60, 1000, 100, 20]),
+            'c_ratio': np.array([0.2, 1, 0, 1, 1]),
+            'h_c': np.array([3, 4, 0, 4, 4]),
+            'w_frac': np.array([0.15, 0.5, 0, 0.5, 0.04]),
+            'remain_frac': np.array([0.0, 0.1, 0, 0, 0]),
+            'q_f_d': np.array([300, 300, 0, 1000, 1000]),
+            't_lim': np.array([15, 20, 0, 25, 25]),
+            'w_frac_h': np.array([0.5, 0.75, 0, 1, 0.5]),
+            'fabr_inrt' : np.array([1000, 1000, 0, 300, 500]),
+            'foo': np.array([1000, 1000, 0, 0, 0])}
+        crit_value = 100.1
         Of_limits = [0.02, 0.2]
         max_fire_duration = 1000
 
@@ -29,7 +29,7 @@ class TestUniEC1(unittest.TestCase):
             Of_limits=Of_limits)
 
     def test_get_relevant_design_fire_indeces(self):
-        npt.assert_equal(self.parametric.relevent_df_indices, np.array([0, 1]))
+        npt.assert_equal(self.parametric.relevent_df_indices, np.array([0, 1, 3, 4]))
         self.assertFalse(self.parametric.is_empty, 'Issue with empty method definition')
 
     def test_get_parameters(self):
@@ -40,122 +40,90 @@ class TestUniEC1(unittest.TestCase):
 
         # Test _calc_comp_sides calculations
         self.parametric._calc_comp_sides()
-        npt.assert_almost_equal(self.parametric.params['c_long'], np.array([14.142, 7.746]), decimal=3)
-        npt.assert_almost_equal(self.parametric.params['c_short'], np.array([2.828, 7.746]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['c_long'], np.array([14.142, 7.746, 10.000, 4.472]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['c_short'], np.array([2.828, 7.746, 10.000, 4.472]), decimal=3)
 
         # Test c_perim
         self.parametric._calc_perimeter()
-        npt.assert_almost_equal(self.parametric.params['c_perim'], np.array([33.941, 30.984]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['c_perim'], np.array([33.941, 30.984, 40.000, 17.889]), decimal=3)
 
         # Test A_t - total enclosure area
         self.parametric._calc_total_area_enclosure()
-        npt.assert_almost_equal(self.parametric.params['A_t'], np.array([181.823, 243.935]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['A_t'], np.array([181.823, 243.935, 360.000, 111.554]), decimal=3)
 
         # Test h_w_eq - average window height
         self.parametric._calc_average_window_height()
-        npt.assert_almost_equal(self.parametric.params['h_w_eq'], np.array([1.500, 3.000]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['h_w_eq'], np.array([1.500, 3.000, 4.000, 2.000]), decimal=3)
 
         # Test A_v - total ventilation area
         self.parametric._calc_total_ventilation_area()
-        npt.assert_almost_equal(self.parametric.params['A_v'], np.array([7.637, 46.476]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['A_v'], np.array([7.637, 46.476, 80.000, 1.431]), decimal=3)
 
         # Test Of - breakage opening factor
         self.parametric._calc_max_open_factor()
         self.parametric._calc_open_factor_breakage()
-        npt.assert_almost_equal(self.parametric.params['Of'], np.array([0.051, 0.297]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['Of'], np.array([0.051, 0.297, 0.444, 0.018]), decimal=3)
 
         # Test apply open factor limits
         self.parametric._apply_open_factor_limits()
-        npt.assert_almost_equal(self.parametric.params['Of'], np.array([0.051, self.parametric.Of_limits[1]]), decimal=3)
+        npt.assert_almost_equal(
+            self.parametric.params['Of'], np.array([0.051, self.parametric.Of_limits[1], self.parametric.Of_limits[1], self.parametric.Of_limits[0]]), decimal=3)
 
         # Test GA - test calc Ga
         self.parametric._calc_GA_factor()
-        npt.assert_almost_equal(self.parametric.params['GA'], np.array([2.225, 33.640]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['GA'], np.array([2.225, 33.640, 373.778, 1.346]), decimal=3)
 
         # Test q_t_d - total surface area fuel density
         self.parametric._calc_total_surface_area_fuel_density()
-        npt.assert_almost_equal(self.parametric.params['q_t_d'], np.array([65.998, 73.790]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['q_t_d'], np.array([65.998, 73.790, 277.778, 179.285]), decimal=3)
 
         # Test t_max_vent - time to max temp for vent controlled fire
         self.parametric._calc_t_max_vent()
-        npt.assert_almost_equal(self.parametric.params['t_max_vent'], np.array([0.257, 0.074]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['t_max_vent'], np.array([0.257, 0.074, 0.278, 1.793]), decimal=3)
 
         # Test t_max_fuel - time to max temp for fuel controlled fire
         self.parametric._calc_t_max_fuel()
-        npt.assert_almost_equal(self.parametric.params['t_max_fuel'], np.array([0.250, 0.333]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['t_max_fuel'], np.array([0.250, 0.333, 0.417, 0.417]), decimal=3)
 
         # Test Of_lim - Open factor for fuel controlled
         self.parametric._calc_open_factor_fuel()
-        npt.assert_almost_equal(self.parametric.params['Of_lim'],  np.array([0.026, 0.022]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['Of_lim'],  np.array([0.026, 0.022, 0.067, 0.043]), decimal=3)
 
         # Test k and Test GA_lim - K factor for GA_lim - NOTE: Ga_lim is modified with k
         self.parametric._calc_GA_lim_factor()
         self.parametric._calc_GA_lim_k_mod()
-        npt.assert_almost_equal(self.parametric.params['k'], np.array([0.995, 0.991]), decimal=3)
-        npt.assert_almost_equal(self.parametric.params['GA_lim'], np.array([0.583, 0.408]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['k'], np.array([0.995, 0.991, 1.000, 1.000]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['GA_lim'], np.array([0.583, 0.408, 41.531, 6.228]), decimal=3)
 
         # Test burning regime - checking for fuel or vent control
         self.parametric._define_burning_regime()
-        npt.assert_equal(self.parametric.params['regime'], np.array(['V', 'F']))
+        npt.assert_equal(self.parametric.params['regime'], np.array(['V', 'F', 'F', 'V']))
 
         # Test max_temp_t - time of maximum temperature 
         self.parametric._calc_max_temp_time()
-        npt.assert_almost_equal(self.parametric.params['max_temp_t'], np.array([0.257, 0.333]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['max_temp_t'], np.array([0.257, 0.333, 0.417, 1.793]), decimal=3)
 
         # Test t_star_max
         self.parametric._calc_t_star_max()
-        npt.assert_almost_equal(self.parametric.params['t_str_max_heat'], np.array([0.571, 0.136]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['t_str_max_heat'], np.array([0.571, 0.136, 17.305, 2.412]), decimal=3)
 
         # Test max_temp - maximum temperature
         self.parametric._calc_max_temp()
-        npt.assert_almost_equal(self.parametric.params['max_temp'], np.array([859.632, 665.721]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['max_temp'], np.array([859.632, 665.721, 1331.519, 1075.543]), decimal=3)
 
         # Test fire duration - fire duration TODO: Add expected values for fire duration
         self.parametric._calc_fire_duration()
-        npt.assert_almost_equal(self.parametric.params['burnout'], np.array([52.676, 24.607]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['burnout'], np.array([52.676, 24.607, 25.842, 295.837]), decimal=3)
 
         # Test _define_max_gas_temp - for external class use
         self.parametric._define_max_gas_temp()
-        npt.assert_almost_equal(self.parametric.params['max_gas_temp'], np.array([859.632, 665.721]), decimal=3)
+        npt.assert_almost_equal(self.parametric.params['max_gas_temp'], np.array([859.632, 665.721, 1331.519, 1075.543]), decimal=3)
 
+    def test_get_exposure(self):
+        # test get exposure method with the following times: 0, 5, 30, 60, 10000
+        # test subsample masking with few numbers
+        pass
 
-
-    # test temperature for heating phase - revisit
-    # def test_calc_heat_phase_temp(self):
-    #     self.parametric.perform_initial_calculations()
-    #
-    #
-    #     expected_heat_phase_temp = {3: [626.0, 232.0],
-    #                                 5: [716.0, 336.0]}
-    #
-    #     for key in expected_heat_phase_temp:
-    #         self.parametric.perform_initial_calculations()
-    #
-    #         expected_result = expected_heat_phase_temp.get(key)
-    #
-    #         t = key
-    #         _, t_str_heat, _ = self.parametric.get_exposure(t)
-    #
-    #         npt.assert_almost_equal(self.parametric._calc_heat_phase_temp(t_str_heat), expected_result, err_msg=f"error for t = {key}", decimal=0)
-    #
-    # # Test for get exposure
-    # def test_get_exposure(self):
-    #     self.parametric.perform_initial_calculations()
-    #
-    #     # test cool phase temp
-    #     expected_cool_phase_temp = {20: [756.0, 666.0],
-    #                                 50: [80.0, 20.0]}
-    #
-    #     for key in expected_cool_phase_temp:
-    #
-    #         self.parametric.perform_initial_calculations()
-    #
-    #         expected_result = expected_cool_phase_temp.get(key)
-    #
-    #         t = key
-    #         _, _, cool_phase_temp = self.parametric.get_exposure(t)
-    #
-    #         npt.assert_almost_equal(cool_phase_temp, expected_result, err_msg=f"error at t = {t}", decimal=0)
 
     #TODO check bad samples - to be updated once such tests are created
 
