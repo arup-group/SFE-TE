1b7999de7dc3f852b488cb9ae36eda8a1e79772d
Yavor Panev <yavor.panev@arup.com>
2023-10-03 12:23:23 +0100

    updated start for AWF

--- quick_test_awf.bat
+++ quick_test_awf.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd %~dp0
 call conda activate te_awf_dev
-python src/main.py -i sample_inputs/quick_test.json -o src/dump
+python te_awf_start.py -i sample_inputs/quick_test.json -o src/dump
 call conda deactivate
 pause
